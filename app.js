15bfe1ff0804541884819f1800a07a4d54d30751
sergokiko <sergokiko@gmail.com>
2020-11-26 17:16:03 +0200

    semifinal

--- app.js
+++ app.js
@@ -1,49 +1,86 @@
+let users = [
+    {name: 'Sergiy', email: 'sergokkio@gmail.com', password: 'ababagalamaga'},
+    {name: 'Olena', email: 'olenaromaonva@mail.ru', password: '12345678'},
+    {name: 'Ser', email: 'ser@mail.com', password: '1234'},
+]
+
+
 const fs = require('fs')
 const path = require('path')
+const express = require('express')
+const expressHbs = require('express-handlebars')
 
- const homeWorkDir2000 = path.join(process.cwd(), 'DZ', '2000')
-const homeWorkDir1800 = path.join(process.cwd(), 'DZ', '1800')
+const app = express();
 
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
 
+const viewsPath = path.join(process.cwd(), 'views')
 
-moveFilesBetweenDir(homeWorkDir2000,homeWorkDir1800)
-moveFilesBetweenDir(homeWorkDir1800,homeWorkDir2000)
+app.use(express.static(viewsPath))
 
-
-function moveFilesBetweenDir ( startDir, finishDir) {
-    fs.readdir((startDir), ((err, res) => {
-        if (err){
-            return  console.log(err);
-        }
-        res.forEach(file =>{
-            fs.rename(path.join(startDir, file), path.join(finishDir, file), (err1 => console.log(err1)) )
-        })
-
-    }))
-}
-
-const dirForRecursiveDelete = path.join(process.cwd(), 'recurcive-delete');
-
-recursiveDelete(dirForRecursiveDelete)
-
-function recursiveDelete (dirPath) {
-        fs.readdir(dirPath, ((err, res) => {
-            console.log(res);
-            res.forEach(element => {
-              const currentPath = path.join(dirPath, element)
-              fs.stat(currentPath, (err, stats) => {
-                  if (stats.isDirectory()) {
-                      recursiveDelete(currentPath)
-                  } else {
-                      fs.unlink((currentPath), err1 => err1 ? console.log(err1) : null)
-                     }
-                 })
-
-                })
-
-        }))
-    fs.rmdir(dirPath, err1 => err1? console.log(err1): null)
-}
+app.set('view engine', '.hbs');
+app.engine('.hbs', expressHbs({defaultLayout: false}));
+app.set('views', viewsPath)
 
 
+let isLogged = false;
+let currentUserName = ''
 
+
+app.get('/', ((req, res) => {
+    res.render('main', {isLogged})
+}))
+
+app.get('/users', ((req, res) => {
+    isLogged ? (res.render('users', {users, currentUserName})) : (res.render('errPage'))
+
+}))
+
+
+app.get('/login', ((req, res) => {
+    res.render('login')
+}))
+
+app.post('/login', (req, res) => {
+    const {email, password, name} = req.body;
+    const isUserExist = !!users.find(el => el.email === email && el.password === password);
+    if (!isUserExist) {
+        res.redirect('/error')
+        return
+    }
+    currentUserName = name
+    isLogged = true
+    res.redirect('/users')
+})
+
+app.post('/logout', ((req, res) => {
+    isLogged = false;
+    res.redirect('/login')
+}))
+
+app.get('/auth', ((req, res) => {
+    isLogged = false
+        res.render('auth')
+}))
+
+app.post('/auth', ((req, res) => {
+    const {email} = req.body
+     const isEmailExist = !!users.find(el => el.email === email);
+    if(isEmailExist){
+        res.redirect('/error')
+        return
+    }
+    users.push(req.body)
+    res.redirect('/login')
+}))
+
+
+
+app.get('/error', ((req, res) => {
+    res.render('errPage')
+}))
+
+app.listen(5000, () => {
+    console.log('app listen 5000')
+})
