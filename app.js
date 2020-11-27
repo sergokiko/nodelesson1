04f34b083aed8d612ff914af421ecd6f35320b4f
sergokiko <sergokiko@gmail.com>
2020-11-27 15:08:18 +0200

    semifinal

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
-let users = [
-    {name: 'Sergiy', email: 'sergokkio@gmail.com', password: 'ababagalamaga'},
-    {name: 'Olena', email: 'olenaromaonva@mail.ru', password: '12345678'},
-    {name: 'Ser', email: 'ser@mail.com', password: '1234'},
-]
-
-
 const fs = require('fs')
 const path = require('path')
 const express = require('express')
-const expressHbs = require('express-handlebars')
-
+const expressHbs = require('express-handlebars');
 const app = express();
+const viewsPath = path.join(process.cwd(), 'views')
+const jsonUsersPath = path.join(process.cwd(), './users.json')
+
+let isLogged = false;
+let currentUserName = ''
+let errorMsg = ''
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
-
-const viewsPath = path.join(process.cwd(), 'views')
-
 app.use(express.static(viewsPath))
 
 app.set('view engine', '.hbs');
@@ -24,19 +19,18 @@ app.engine('.hbs', expressHbs({defaultLayout: false}));
 app.set('views', viewsPath)
 
 
-let isLogged = false;
-let currentUserName = ''
-
-
 app.get('/', ((req, res) => {
     res.render('main', {isLogged})
 }))
 
-app.get('/users', ((req, res) => {
 
-    isLogged ? (res.render('users', {users, currentUserName})) : (res.render('errPage'))
+app.get('/users', (req, res) => {
+    fs.readFile(jsonUsersPath, ((err, data) => {
+        const users = JSON.parse(data.toString());
 
-}))
+        isLogged ? (res.render('users', {users, currentUserName})) : (res.render('errPage'))
+    }))
+})
 
 
 app.get('/login', ((req, res) => {
@@ -45,44 +39,64 @@ app.get('/login', ((req, res) => {
 
 app.post('/login', (req, res) => {
     const {email, password} = req.body;
-    const isUserExist = users.find(el => el.email === email && el.password === password);
-    if (!isUserExist) {
-        res.redirect('/error')
-        return
-    }
 
-    currentUserName = isUserExist.name
-    isLogged = true
-    res.redirect('/users')
+    fs.readFile(jsonUsersPath, ((err, data) => {
+        const users = JSON.parse(data.toString());
+        const isUserExist = users.find(el => el.email === email && el.password === password);
+
+        if (!isUserExist) {
+            errorMsg = 'Wrong password or email'
+            res.redirect('/error')
+            return
+        }
+
+        currentUserName = isUserExist.name
+        isLogged = true
+        res.redirect('/users')
+    }))
 })
+
 
 app.post('/logout', ((req, res) => {
     isLogged = false;
     res.redirect('/login')
 }))
 
+
 app.get('/auth', ((req, res) => {
     isLogged = false
-        res.render('auth')
+    res.render('auth')
 }))
 
 app.post('/auth', ((req, res) => {
     const {email} = req.body
-     const isEmailExist = !!users.find(el => el.email === email);
-    if(isEmailExist){
-        res.redirect('/error')
-        return
-    }
-    users.push(req.body)
-    res.redirect('/login')
+
+    fs.readFile(jsonUsersPath, ((err, data) => {
+
+        const users = JSON.parse(data.toString());
+        const isEmailExist = users.find(el => el.email === email);
+
+        if (isEmailExist) {
+            errorMsg = 'User with current email already exist'
+            res.redirect('/error')
+            return
+        }
+
+        users.push(req.body)
+        res.redirect('/login')
+        fs.writeFile(jsonUsersPath, JSON.stringify(users), err => err && null)
+
+    }))
 }))
 
 
-
 app.get('/error', ((req, res) => {
-    res.render('errPage')
+    res.render('errPage', {errorMsg})
 }))
 
 app.listen(5000, () => {
     console.log('app listen 5000')
 })
+
+
+
