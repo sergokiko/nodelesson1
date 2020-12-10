9fa3d1af6630afba8fa885b9bf74ebdf6ef70df7
sergokiko <sergokiko@gmail.com>
2020-12-10 21:09:04 +0200

    homework 5

--- src/database/migrations/20201210124615-create-car-table.js
+++ src/database/migrations/20201210124615-create-car-table.js
@@ -42,8 +42,6 @@ module.exports = {
                     model: 'user',
                     key: 'id'
                 },
-                onDelete: 'CASCADE',
-                onUpdate: 'CASCADE',
             }
         });
     },
